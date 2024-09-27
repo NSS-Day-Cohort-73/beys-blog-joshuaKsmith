d68b1aaaed2279104460f22e708823c8662b0bac
unknown <mtgmirage@protonmail.com>
2024-09-27 13:45:07 -0500

    a few rendering/css concerns addressed

--- scripts/BeyBlog.js
+++ scripts/BeyBlog.js
@@ -3,20 +3,22 @@ import { Articles } from "./articles.js"
 import { Categories } from "./categories.js"
 
 export const BeyBlog = () => {
-    //const mainHTML =
+
     return `
-        <article class="authors column">
-            <h2>Authors</h2>
-            ${Authors()}
-        </article>
-        <article class="articles column">
-            <h2>Articles</h2>
-            ${Articles()}
-        </article>
-        <article class="categories column">
+        <div class="top_row">
+            <article class="authors_column">
+                <h2>Authors</h2>
+                ${Authors()}
+            </article>
+            <article class="articles_column">
+                <h2>Articles</h2>
+                ${Articles()}
+            </article>
+        </div>
+        <article class="categories_column">
             <h2>Categories</h2>
             ${Categories()}
         </article>
     `
-    //return mainHTML
+
 }
